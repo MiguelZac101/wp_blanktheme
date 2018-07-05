7e37aa14e0a57ed3b00ef9b828eb23b3d3479e8b
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-05 17:12:05 -0500

    221. Editando el CSS para nuestra imagen destacada.

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -2,9 +2,11 @@
 <html <?php language_attributes(); ?> class="no-js">
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
-    echo ' :';
-} ?> <?php bloginfo('name'); ?></title>
+        <title><?php wp_title(''); ?><?php
+            if (wp_title('', false)) {
+                echo ' :';
+            }
+            ?> <?php bloginfo('name'); ?></title>
 
         <link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
@@ -31,32 +33,35 @@
 
         <!-- header -->
         <header class="header clear" role="banner">
-            <div class="wrapper">
+            <div class="navegacion">
+                <div class="wrapper">
 
+                    <!-- logo -->
+                    <div class="logo">
+                        <a href="<?php echo home_url(); ?>">
+                            <!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+                        </a>
+                    </div>
+                    <!-- /logo -->
 
-                <!-- logo -->
-                <div class="logo">
-                    <a href="<?php echo home_url(); ?>">
-                        <!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-                    </a>
+                    <!-- nav -->
+                    <nav class="nav" role="navigation">
+                        <?php html5blank_nav(); ?>
+                    </nav>
+                    <!-- /nav -->
                 </div>
-                <!-- /logo -->
-
-                <!-- nav -->
-                <nav class="nav" role="navigation">
-<?php html5blank_nav(); ?>
-                </nav>
-                <!-- /nav -->
             </div>
+            <?php if (is_page()) { ?>
+                <?php $destacada = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full'); ?>
+                <?php $destacada = $destacada[0]; ?>
+                <div class="imagenFondo" style="background-image: url(<?php echo $destacada; ?>)"></div>
+<?php } ?>
+
+
         </header>
         <!-- /header -->
-        
-        <?php if(is_page()){?>
-            <?php $destacada = wp_get_attachment_image_src(get_post_thumbnail_id(),'full'); ?>
-            <?php $destacada = $destacada[0]; ?>
-            <div class="imagenFondo" style="background-image: url(<?php echo $destacada; ?>)"></div>
-        <?php } ?>
-        
+
+
         <!-- wrapper -->
         <div class="wrapper">
