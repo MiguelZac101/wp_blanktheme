d3a460d3b787801773540eba73a019b451f160de
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-12 17:16:37 -0500

    230. Imprimiendo todos los campos personalizados a nuestra planttilla.

--- wp-content/themes/html5blank-stable/functions.php
+++ wp-content/themes/html5blank-stable/functions.php
@@ -28,6 +28,8 @@ if (function_exists('add_theme_support'))
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
     add_image_size('mediano', 350,250, true); //Mediano
+    add_image_size('toursDestacado', 523,294, true); //toursdestacado
+
 
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
--- wp-content/themes/html5blank-stable/page-tours.php
+++ wp-content/themes/html5blank-stable/page-tours.php
@@ -11,7 +11,7 @@
 
         <h1><span><?php the_title(); ?></span></h1>
 
-        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <?php if (have_posts()):?>
                 <?php
                 $args = array(
                     'post_type' => 'tours',
@@ -25,12 +25,27 @@
 
                     <!-- article -->
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <?php the_title(); ?>
-
+                        <?php the_post_thumbnail('toursDestacado'); ?>
+                        <a href="<?php the_permalink(); ?>">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masinfo.png"/>
+                        </a>
+                        <a href="<?php the_permalink(); ?>">
+                            <h2><?php the_title(); ?></h2>
+                        </a>
+                        <?php 
+                            $formato = 'd F, Y';
+                            $fecha = strtotime(get_field('fecha_de_salida'));
+                            $fechadesalida = date_i18n($formato,$fecha);
+                            
+                            $fecharetorno = strtotime(get_field('fecha_de_retorno'));
+                            $fechaderetorno = date_i18n($formato,$fecharetorno);
+                        ?>
+                        <p class="fecha"><?php echo $fechadesalida; ?> - <?php echo $fechaderetorno; ?></p>
+                        <p class="precio"><?php the_field('precio'); ?></p>
+                        <p><?php the_field('descripcion_corta'); ?></p>
                     </article>
                     <!-- /article -->               
-                <?php endwhile; wp_reset_postdata(); ?>
-            <?php endwhile; ?>
+                <?php endwhile; wp_reset_postdata(); ?>            
 
         <?php else: ?>
 
@@ -42,7 +57,7 @@
             </article>
             <!-- /article -->
 
-<?php endif; ?>
+        <?php endif; ?>
 
     </section>
     <!-- /section -->
