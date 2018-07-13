f8f7d6f808fa54cdebbe1effffe73c2763ddcb9f
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-13 17:52:18 -0500

    231. Creando los estilos CSS para nuestra sección de Tourss.

--- wp-content/themes/html5blank-stable/page-tours.php
+++ wp-content/themes/html5blank-stable/page-tours.php
@@ -11,43 +11,50 @@
 
         <h1><span><?php the_title(); ?></span></h1>
 
-        <?php if (have_posts()):?>
-                <?php
-                $args = array(
-                    'post_type' => 'tours',
-                    'posts_per_page' => 4,
-                    'order' => 'DESC',
-                    'orderby' => 'date'
-                );
-                ?>
-                <?php $tours = new WP_Query($args); ?>
-                <?php while ($tours->have_posts()): $tours->the_post(); ?>
+        <?php if (have_posts()): ?>
+            <?php
+            $args = array(
+                'post_type' => 'tours',
+                'posts_per_page' => 4,
+                'order' => 'DESC',
+                'orderby' => 'date'
+            );
+            ?>
+            <?php $tours = new WP_Query($args); ?>
+            <?php while ($tours->have_posts()): $tours->the_post(); ?>
 
-                    <!-- article -->
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <!-- article -->
+                <article id="post-<?php the_ID(); ?>" <?php post_class('grid2-4'); ?>>
+                    <div class="imagen-destacada">
                         <?php the_post_thumbnail('toursDestacado'); ?>
-                        <a href="<?php the_permalink(); ?>">
+                        <a class="mas-info" href="<?php the_permalink(); ?>">
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/masinfo.png"/>
                         </a>
-                        <a href="<?php the_permalink(); ?>">
-                            <h2><?php the_title(); ?></h2>
-                        </a>
-                        <?php 
-                            $formato = 'd F, Y';
-                            $fecha = strtotime(get_field('fecha_de_salida'));
-                            $fechadesalida = date_i18n($formato,$fecha);
-                            
-                            $fecharetorno = strtotime(get_field('fecha_de_retorno'));
-                            $fechaderetorno = date_i18n($formato,$fecharetorno);
-                        ?>
+                    </div><!--.imagen-destacada-->
+                    <a href="<?php the_permalink(); ?>">
+                        <h2><?php the_title(); ?></h2>
+                    </a>
+                    <?php
+                    $formato = 'd F, Y';
+                    $fecha = strtotime(get_field('fecha_de_salida'));
+                    $fechadesalida = date_i18n($formato, $fecha);
+
+                    $fecharetorno = strtotime(get_field('fecha_de_retorno'));
+                    $fechaderetorno = date_i18n($formato, $fecharetorno);
+                    ?>
+                    <div class="fecha-precio clear">
                         <p class="fecha"><?php echo $fechadesalida; ?> - <?php echo $fechaderetorno; ?></p>
                         <p class="precio"><?php the_field('precio'); ?></p>
-                        <p><?php the_field('descripcion_corta'); ?></p>
-                    </article>
-                    <!-- /article -->               
-                <?php endwhile; wp_reset_postdata(); ?>            
+                    </div>
+                    <div class="clear"></div>
+                    <p><?php the_field('descripcion_corta'); ?></p>
+                </article>
+                <!-- /article -->               
+            <?php endwhile;
+            wp_reset_postdata();
+            ?>            
 
-        <?php else: ?>
+<?php else: ?>
 
             <!-- article -->
             <article>
@@ -57,12 +64,12 @@
             </article>
             <!-- /article -->
 
-        <?php endif; ?>
+<?php endif; ?>
 
     </section>
     <!-- /section -->
 </main>
 
-<?php // get_sidebar();   ?>
+<?php // get_sidebar();     ?>
 
 <?php get_footer(); ?>
