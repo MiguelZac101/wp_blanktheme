54ad77b4afa5a59a7db4b90d22104703dd784a4e
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-17 12:46:52 -0500

    238. Creando un ustom Post Type para la sección de Testimoniales con código.

--- wp-content/themes/html5blank-stable/functions.php
+++ wp-content/themes/html5blank-stable/functions.php
@@ -352,6 +352,8 @@ add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comment
 add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
 add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type
+add_action('init', 'create_testimoniales'); // Add our HTML5 Blank Custom Post Type
+
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 
@@ -421,6 +423,44 @@ function create_post_type_html5()
             'search_items' => __('Search Tours', 'html5blank'),
             'not_found' => __('No Tourss found', 'html5blank'),
             'not_found_in_trash' => __('No Tourss found in Trash', 'html5blank')
+        ),
+        'public' => true,
+        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+        'has_archive' => true,
+        'menu_position' => 6,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail'
+        ), // Go to Dashboard Custom HTML5 Blank post for supports
+        'can_export' => true, // Allows export in Tools > Export
+        'taxonomies' => array(
+            'post_tag',
+            'category'
+        ) // Add Category and Post Tags support
+    ));
+}
+
+function create_testimoniales()
+{
+    register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('post_tag', 'html5-blank');
+    register_post_type('Testimoniales', // Register Custom Post Type
+        array(
+        'labels' => array(
+            'name' => __('Testimoniales', 'html5blank'), // Rename these to suit
+            'singular_name' => __('Testimoniales', 'html5blank'),
+            'add_new' => __('Add New', 'html5blank'),
+            'add_new_item' => __('Add New Testimoniales', 'html5blank'),
+            'edit' => __('Edit', 'html5blank'),
+            'edit_item' => __('Edit Testimoniales', 'html5blank'),
+            'new_item' => __('New Testimoniales', 'html5blank'),
+            'view' => __('View Testimoniales', 'html5blank'),
+            'view_item' => __('View Testimoniales', 'html5blank'),
+            'search_items' => __('Search Testimoniales', 'html5blank'),
+            'not_found' => __('No Testimoniales found', 'html5blank'),
+            'not_found_in_trash' => __('No Testimoniales found in Trash', 'html5blank')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
