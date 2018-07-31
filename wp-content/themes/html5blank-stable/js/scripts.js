39192f8557f0286994e69936ae503dca6f60ec4c
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-31 17:44:54 -0500

    250. Finalizando nuestra plantilla para las Entradas de nuestro blog.

--- wp-content/themes/html5blank-stable/js/scripts.js
+++ wp-content/themes/html5blank-stable/js/scripts.js
@@ -9,6 +9,7 @@
         });
         
         jQuery('.single-tours header nav ul li:contains("Tours")').addClass('current_page_item');
+        jQuery('.single-post header nav ul li:contains("Consejos")').addClass('current_page_item');
 
     });
 
