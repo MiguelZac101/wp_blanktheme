50c0ef011a05a347045425c6dc9a2a974f583224
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-07-31 17:14:45 -0500

    249. Creando la plantilla para cada Entrada de nuesro blog.

--- wp-content/themes/html5blank-stable/single.php
+++ wp-content/themes/html5blank-stable/single.php
@@ -1,67 +1,61 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+<main role="main">
+    <!-- section -->
+    <section class="clear">
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <!-- article -->
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+                    <!-- post thumbnail -->
+                    <?php if (has_post_thumbnail()) : // Check if Thumbnail exists ?>
+                        <div class="foto">
+                            <?php the_post_thumbnail('principalBlog'); // Fullsize image for the single post ?>
+                        </div>				
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+                    <?php endif; ?>
+                    <!-- /post thumbnail -->
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+                    <!-- post title -->
+                    <h1><span><?php the_title(); ?></span></h1>
+                    <!-- /post title -->
 
-			<?php the_content(); // Dynamic Content ?>
+                    <div class="grid1-3">
+                        <!-- post details -->
+                        <span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+                        <span class="author"><?php _e('Published by', 'html5blank'); ?> <?php the_author_posts_link(); ?></span>
+                        <p><?php _e('Categorised in: ', 'html5blank'); the_category(', '); // Separated by commas  ?></p>
+                        <span class="comments"><?php if (comments_open(get_the_ID())) comments_popup_link(__('Leave your thoughts', 'html5blank'), __('1 Comment', 'html5blank'), __('% Comments', 'html5blank')); ?></span>
+                        <!-- /post details -->
+                    </div>
+                    <div class="grid2-3">
+                        <?php the_content(); // Dynamic Content ?>
+                        <?php comments_template(); ?>
+                    </div>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+                </article>
+                <!-- /article -->
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+            <?php endwhile; ?>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+<?php else: ?>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+            <!-- article -->
+            <article>
 
-			<?php comments_template(); ?>
+                <h1><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h1>
 
-		</article>
-		<!-- /article -->
+            </article>
+            <!-- /article -->
 
-	<?php endwhile; ?>
+<?php endif; ?>
 
-	<?php else: ?>
+    </section>
+    <!-- /section -->
+</main>
 
-		<!-- article -->
-		<article>
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
