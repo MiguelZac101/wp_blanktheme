fef5d467f1e45c9691284bcf64be3a2f4f9ef6e1
Miguel Quispe <miguel.quispe@fractal.com.pe>
2018-08-15 15:18:01 -0500

    262. Creando un Slider para nuestra página principal.

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -1,2 +1,28 @@
 <?php get_header(); ?>
+</div>
+<?php
+
+$args = array(
+    'post_type' => 'slider',
+    'posts_per_page' => -1,
+    'order' => 'DESC',
+    'orderby' => 'date'
+);
+?>
+<?php $slider = new WP_Query($args); ?>
+<ul class="slider">
+    
+
+<?php while ($slider->have_posts()): $slider->the_post(); ?>
+    <li>
+        <a href="<?php the_field('url'); ?>">
+            <?php the_post_thumbnail('slider'); ?>
+        </a>
+    </li>
+<?php
+endwhile;
+wp_reset_postdata();
+?>      
+    </ul>
+<div class="wrapper">
 <?php get_footer(); ?>
--- wp-content/themes/html5blank-stable/js/scripts.js
+++ wp-content/themes/html5blank-stable/js/scripts.js
@@ -7,7 +7,10 @@
         jQuery('.gallery a').each(function(){
            jQuery(this).attr({'data-lightbox':'galeria'}) ;
         });
-        
+        jQuery('ul.slider').bxSlider({
+            mode: 'fade',
+            pager: false
+        });
         jQuery('.single-tours header nav ul li:contains("Tours")').addClass('current_page_item');
         jQuery('.single-post header nav ul li:contains("Consejos")').addClass('current_page_item');
 
